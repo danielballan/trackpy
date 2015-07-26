0149d54864188bc6318e63ea95b691a93f4b834f
Dan Allan <daniel.b.allan@gmail.com>
2015-07-26 16:39:17 -0400

    Revert "Faster bandpass"

--- trackpy/diag.py
+++ trackpy/diag.py
@@ -18,6 +18,11 @@ def performance_report():
     else:
         print("SLOW: numba was not found")
 
+    if preprocessing.USING_FFTW:
+        print("FAST: Using pyfftw for image preprocessing.")
+    else:
+        print("SLOW: pyfftw not found (slower image preprocessing).")
+
 
 def dependencies():
     """
